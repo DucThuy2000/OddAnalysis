a3a8a35f57b04a13f72e6a89974b963e6e49a279
Darwin <ducthuy1705@gmail.com>
2024-04-24 00:14:21 +0700

    feat: update odds after nav to tab 1

--- src/app/shared/constants/odds.ts
+++ src/app/shared/constants/odds.ts
@@ -2,6 +2,6 @@ import { EOdds, ICommonSelection } from '@shared/models';
 
 export const ODDS = [
   { value: EOdds.GOALS, label: EOdds.GOALS },
-  { value: EOdds.CONNERS, label: EOdds.CONNERS },
+  { value: EOdds.CORNERS, label: EOdds.CORNERS },
   { value: EOdds.CARDS, label: EOdds.CARDS },
 ];
